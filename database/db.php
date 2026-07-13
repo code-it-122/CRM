819f6268e3b0ebf3f9599e35fb071e5a98f30ff5
vaishnavi <vaishnavishendge30@gmail.com>
2026-07-13 20:25:17 +0530

    db changes

--- database/db.php
+++ database/db.php
@@ -4,10 +4,4 @@ $username="root";
 $password="";
 $database="crm_db";
 $conn=mysqli_connect($servername,$username,$password,$database);
-
-/*$servername="sql207.infinityfree.com";
-$username="if0_42377797";
-$password="vaishnavicrm123";
-$database="if0_42377797_crm_db";
-$conn=mysqli_connect($servername,$username,$password,$database);*/
 ?>
